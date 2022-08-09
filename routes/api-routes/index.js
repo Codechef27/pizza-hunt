9cca3e710266caba09cf18eeb3d2a202b5f5093c
BradleyK <bradleyjkennett@gmail.com>
2022-08-09 08:45:28 -0400

    comment add

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
+
 
 
 
 router.use('/pizzas', pizzaRoutes);
+router.use('/comments', commentRoutes);
 
 
 module.exports = router;
